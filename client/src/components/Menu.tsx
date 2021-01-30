2539cf15f4853d9ac3703b9a939fe17339e876f5
EleMint <dylanschmidt0216@gmail.com>
2021-01-30 00:49:24 -0600

    Fix styles; only show when authenticated

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+
+import { useAccount } from '../contexts/Account';
 import { DraggableVerticalSeparator } from './DraggableVerticalSeparator';
 
 type HamburgerProps = {
@@ -31,16 +33,16 @@ const HamburgerBase = styled.div<HamburgerProps>`
     display: inline-block;
     cursor: pointer;
 
-    .change .bar1 {
+    .change.bar1 {
         -webkit-transform: rotate(-45deg) translate(-9px, 6px);
         transform: rotate(-45deg) translate(-9px, 6px);
     }
 
-    .change .bar2 {
+    .change.bar2 {
         opacity: 0;
     }
 
-    .change .bar3 {
+    .change.bar3 {
         -webkit-transform: rotate(45deg) translate(-8px, -8px);
         transform: rotate(45deg) translate(-8px, -8px);
     }
@@ -51,37 +53,39 @@ const HamburgerBar = styled.div`
     height: 5px;
     background-color: #333;
     margin: 6px 0;
-    transition: 0.4s;
+    transition: 0.5s;
 `
 
 export const Menu = () => {
     const [open, setOpen] = React.useState<boolean>(false)
     const [width, setWidth] = React.useState<number>(0)
 
+    const { isAuthenticated, isLoading } = useAccount()
+
     const onHamburgerClick = () => {
         setOpen(!open)
         setWidth(!open ? 30 : 0)
     }
 
-    React.useEffect(() => console.log(open), [open])
+    return !isLoading && isAuthenticated
+        ? (
+            <>
+                <Hamburger
+                    onClick={onHamburgerClick}
+                    open={open}
+                    width={width}
+                />
+                { (open && <DraggableVerticalSeparator
+                    width={width}
+                    setWidth={setWidth}
+                    onDoubleClick={() => open && onHamburgerClick()}
+                />) || <></>}
+                <Base width={width}>
 
-    return (
-        <>
-            <Hamburger
-                onClick={onHamburgerClick}
-                open={open}
-                width={width}
-            />
-            { (open && <DraggableVerticalSeparator
-                width={width}
-                setWidth={setWidth}
-                onDoubleClick={() => open && onHamburgerClick()}
-            />) || <></>}
-            <Base width={width}>
-
-            </Base>
-        </>
-    )
+                </Base>
+            </>
+        )
+        : <></>
 }
 
 const Base = styled.div<{ width: number }>`
